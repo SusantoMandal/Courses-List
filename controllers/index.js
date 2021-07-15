3865cfc5fa8c803e7d533016bd6aeda246a97299
susanto <susantomandal1998@gmail.com>
2021-07-15 18:22:23 +0530

    added esliint format

--- controllers/index.js
+++ controllers/index.js
@@ -1,94 +1,91 @@
 const express = require('express');
-const router = express.Router();
-const path = require('path');
-const jsPath = path.join(__dirname, '..', 'model', 'dbService.js');
-const dbService = require(jsPath);
-const date = new Date();
+const dbService = require('../model/dbService');
 
+const router = express.Router();
+const date = new Date();
 
 /* GET ROUTES
  */
-router.get('/',(req,res)=>{
-    res.send('<h1>Course List</h1>');
+router.get('/', (req, res) => {
+  res.send('<h1>Course List</h1>');
 });
 
-router.get('/course',(req,res)=>{
-    if(req.query.getAll == undefined)
-        res.send(dbService.fetchData());
-    else {
-        let data = req.query.getAll;
-        let gather = [];
-        dbService.fetchData().forEach(element => {
-            gather.push(element[data]);
-        });
-        res.send(gather);
-    }
+router.get('/course', (req, res) => {
+  if (req.query.getAll === undefined) res.send(dbService.fetchData());
+  else {
+    const data = req.query.getAll;
+    const gather = [];
+    dbService.fetchData().forEach((element) => {
+      gather.push(element[data]);
+    });
+    res.send(gather);
+  }
 });
 
-router.get('/course/:courseID',(req,res)=>{
-    const present = dbService.fetchData().find(c => c.courseID == parseInt(req.params.courseID));
-    if(!present) {
-        return res.status(404).send(`COURSE ID ${req.params.courseID} is not present`);
-    } 
-    res.send(present);
-})
-
+router.get('/course/:courseID', (req, res) => {
+  const present = dbService.fetchData().find((c) => c.courseID === req.params.courseID);
+  if (!present) {
+    return res.status(404).send(`COURSE ID ${req.params.courseID} is not present`);
+  }
+  res.send(present);
+  return true;
+});
 
 /* POST ROUTES
  */
-router.post('/course',(req,res)=>{
-    const present = dbService.fetchData().find(c => c.courseID == parseInt(req.body.courseID));
-    if(present) return res.status(404).send(`COURSE ID ${req.body.courseID} is already present`);
+router.post('/course', (req, res) => {
+  const present = dbService.fetchData().find((c) => c.courseID === req.body.courseID);
+  if (present) return res.status(404).send(`COURSE ID ${req.body.courseID} is already present`);
 
-    const result = dbService.validationPost(req.body);
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+  const result = dbService.validationPost(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return true;
+  }
 
-    req.body.createdAt = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    req.body.modifiedAt = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    dbService.fetchData().push(req.body)
-    dbService.updateData(dbService.fetchData());
-    res.send(`COURSE ID ${req.body.courseID} ADDED`);
+  req.body.createdAt = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  req.body.modifiedAt = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  dbService.fetchData().push(req.body);
+  dbService.updateData(dbService.fetchData());
+  res.send(`COURSE ID ${req.body.courseID} ADDED`);
+  return true;
 });
-
 
 /* PUT ROUTES
  */
 
-router.put('/course/:courseID',(req,res)=>{
-    const present = dbService.fetchData().find(c => c.courseID == parseInt(req.params.courseID));
-    if(!present) return res.status('404').send(`CourseID ${req.params.courseID} is not present`);
+router.put('/course/:courseID', (req, res) => {
+  const present = dbService.fetchData().find((c) => c.courseID === req.params.courseID);
+  if (!present) return res.status('404').send(`CourseID ${req.params.courseID} is not present`);
 
-    const result = dbService.validationPut(req.body);
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+  const result = dbService.validationPut(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return true;
+  }
 
-    // present.courseID = (req.body.courseID===undefined) ? present.courseID : req.body.courseID;
-    present.courseName = req.body.courseName;
-    present.subject = req.body.subject;
-    present.modifiedAt = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  // present.courseID = (req.body.courseID===undefined) ? present.courseID : req.body.courseID;
+  present.courseName = req.body.courseName;
+  present.subject = req.body.subject;
+  present.modifiedAt = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
-    dbService.updateData(dbService.fetchData());
-    res.send(`COURSE ID ${present.courseID} UPDATED`);
+  dbService.updateData(dbService.fetchData());
+  res.send(`COURSE ID ${present.courseID} UPDATED`);
+  return true;
 });
-
-
 
 /* DELETE ROUTES
  */
 
-router.delete('/course/:courseID',(req,res)=>{
-    const present = dbService.fetchData().find(c => c.courseID == parseInt(req.params.courseID));
-    if(!present) return res.status('404').send(`CourseID ${req.params.courseID} is not present`);
+router.delete('/course/:courseID', (req, res) => {
+  const present = dbService.fetchData().find((c) => c.courseID === req.params.courseID);
+  if (!present) return res.status('404').send(`CourseID ${req.params.courseID} is not present`);
 
-    const index = dbService.fetchData().indexOf(present);
-    dbService.fetchData().splice(index,1);
-    dbService.updateData(dbService.fetchData());
-    res.send(`COURSE ID ${req.params.courseID} DELETED`);
+  const index = dbService.fetchData().indexOf(present);
+  dbService.fetchData().splice(index, 1);
+  dbService.updateData(dbService.fetchData());
+  res.send(`COURSE ID ${req.params.courseID} DELETED`);
+  return true;
 });
 
 module.exports = router;
--- model/dbService.js
+++ model/dbService.js
@@ -1,42 +1,43 @@
+/* eslint-disable no-console */
 const path = require('path');
-const jsonPath = path.join(__dirname, '..', 'util', 'course.json');
-const utilData = require(jsonPath);
-const fs = require('fs');
 const Joi = require('joi');
+const fs = require('fs');
+const utilData = require('../util/course.json');
 
-function getData(){
-    return utilData;
-};
+const jsonPath = path.join(__dirname, '..', 'util', 'course.json');
 
-function writeData(data){
-    
-    fs.writeFile(jsonPath,JSON.stringify(data),(error,body)=>{
-        if(error)
-            console.log(error);
-    });
+function getData() {
+  return utilData;
+}
+
+function writeData(data) {
+  fs.writeFile(jsonPath, JSON.stringify(data), (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
 }
 
 function validPost(data) {
-    const schema = {
-        courseID: Joi.string().regex(/^[0-9]+$/).required(),
-        courseName: Joi.string().min(4).required(),
-        subject: Joi.string().min(4).required()
-    };
-    return Joi.validate(data, schema);
+  const schema = {
+    courseID: Joi.string().regex(/^[0-9]+$/).required(),
+    courseName: Joi.string().min(4).required(),
+    subject: Joi.string().min(4).required(),
+  };
+  return Joi.validate(data, schema);
 }
 
 function validPut(data) {
-    const schema = {
-        courseName: Joi.string().required(),
-        subject: Joi.string().required()
-    };
-    return Joi.validate(data, schema);
+  const schema = {
+    courseName: Joi.string().required(),
+    subject: Joi.string().required(),
+  };
+  return Joi.validate(data, schema);
 }
 
 module.exports = {
-    fetchData : getData,
-    updateData : writeData,
-    validationPost : validPost,
-    validationPut : validPut
+  fetchData: getData,
+  updateData: writeData,
+  validationPost: validPost,
+  validationPut: validPut,
 };
-
